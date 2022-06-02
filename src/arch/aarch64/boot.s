12afef97f5d8ce4efafcacf26d65970e257379bd
Zang Leigang <zang.lg@outlook.com>
2022-06-02 11:48:48 +0800

    initialize system register

--- src/arch/aarch64/boot.s
+++ src/arch/aarch64/boot.s
@@ -1,3 +1,5 @@
+// Ref1: cortex_a72_mpcore_trm_100095_0003_06_en.pdf
+
 .section .text.boot
 .globl   _start
 
@@ -7,34 +9,76 @@ _start:
 	and  x19, x19, #3
 	cbnz x19, halt
 
-	// 1. TODO: initialize system register
-	// 2. switch to EL1
-	// 3. TODO: setup page table, enable mmu
-	// 4. TODO: clear bss, setup bootstack
-el_setup:
+	b primary_entry
+
+primary_entry:
+	bl init_kernel_el
+	bl __cpu_setup
+	b  __primary_switched
+
+init_kernel_el:
+	mrs  x0, CurrentEL
+	cmp  x0, 0b1000    // EL2 - BIT[2:3]
+	b.eq init_el2
+
+init_el1:
+	// Reset Value, Little Endian + MMU Disabled - Ref1(4.3.30)
+	ldr x0, =0xC50838
+	msr sctlr_el1, x0
+	isb
+
+	ret
+
+init_el2:
 	// use SP_ELx for Exception level ELx
-	msr SPsel, #1
+	msr SPsel, 0x1
 
-	// current exception level
-	mrs x0, CurrentEL
-	and x0, x0, #0b1100
-	lsr x0, x0, #2
+	// HCR_RW - BIT(31): Register Width, EL1 is AArch64 - Ref1(4.3.34)
+	ldr x0, =0x80000000
+	msr hcr_el2, x0
+	isb
 
-el2_setup:
-	// switch to EL1 if we're in EL2
-	cmp x0, #1
-	beq el1_setup
+	// Reset Value, Little Endian + MMU Disabled - Ref1(4.3.30)
+	ldr x0, =0x30C50838
+	msr sctlr_el2, x0
+	isb
 
-	// at EL2, switch to EL1
-	adr x0, el1_setup
-	msr elr_el2, x0
+	// Allow Non-secure EL1 and EL0 to access physical timer and counter.
+	// Enable EL1 physical timers and Clear virtual offset
+	ldr x0, =0x3
+	msr cnthctl_el2, x0
+	msr cntvoff_el2, xzr
+
+	// Disable CP15 traps to EL2 - Ref1(4.3.36)
+	msr hstr_el2, xzr
+
+	// virtual cpu id registers - Ref1(4.3.28, 4.3.29)
+	mrs x0, midr_el1
+	mrs x1, mpidr_el1
+	msr vpidr_el2, x0
+	msr vmpidr_el2, x1
+
+	// No trap of CPACR, FP and SIMD access to EL2 - Ref1(4.3.35)
+	ldr x0, =0x33ff
+	msr cptr_el2, x0
+
+	// Reset Value, Little Endian + MMU Disabled - Ref1(4.3.30)
+	ldr x0, =0xC50838
+	msr sctlr_el1, x0
+	isb
+
+	// Switch to EL1
+	msr elr_el2, lr
 	eret
 
-el1_setup:
+	// TODO
+__cpu_setup:
+	ret
+
+__primary_switched:
 	// setup bootstack
-	adrp x1, __kernel_start
-	sub  x1, x1, #8
-	mov  sp, x1
+	adr x0, __kernel_start
+	mov sp, x0
 
 	// goto rust world
 	b rust_main
